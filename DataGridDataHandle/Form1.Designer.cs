759078ff15e5c548271d6505c5326f16ba6ffc29
sungman ko <33561581+sungmanko@users.noreply.github.com>
2018-12-26 18:45:05 +0900

    DataGridDataHandle
    https://social.msdn.microsoft.com/Forums/vstudio/ko-KR/fae2b039-0df0-42f8-9f6d-8e6d1cf7eb79/cellvaluechanged-erowindex-?forum=visualcsharpko

---
+++ DataGridDataHandle/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SalesGV = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.SalesGV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SalesGV
+            // 
+            this.SalesGV.AllowUserToOrderColumns = true;
+            this.SalesGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.SalesGV.Location = new System.Drawing.Point(12, 12);
+            this.SalesGV.Name = "SalesGV";
+            this.SalesGV.RowTemplate.Height = 21;
+            this.SalesGV.Size = new System.Drawing.Size(728, 150);
+            this.SalesGV.TabIndex = 0;
+            this.SalesGV.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.SalesGV_CellValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(752, 187);
+            this.Controls.Add(this.SalesGV);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.SalesGV)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView SalesGV;
+    }
+}
+
---
+++ DataGridDataHandle/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        DataTable table = new DataTable();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            table.Columns.Add("프로젝트명", typeof(string));
+            table.Columns.Add("수량", typeof(int));
+            table.Columns.Add("단가", typeof(int));
+            table.Columns.Add("금액", typeof(int));
+            table.Columns.Add("부가세", typeof(int));
+            table.Columns.Add("합계", typeof(int));
+
+            this.SalesGV.DataSource = table;
+        }
+
+        private void SalesGV_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            //MessageBox.Show(e.RowIndex.ToString());
+            int q = 0;
+            int p = 0;
+
+            for (int i = 0; i <= e.RowIndex; i++)
+            {
+                if (e.RowIndex >= 0)
+                {
+                    //수량
+                    if ((string)SalesGV.Rows[i].Cells[1].Value.ToString() == "")
+                    {
+                        SalesGV.Rows[i].Cells[1].Value = 0;
+                    }
+                    else
+                    {
+                        p = Convert.ToInt32(SalesGV.Rows[i].Cells[1].Value);
+                    }
+
+                    //단가
+                    if ((string)SalesGV.Rows[i].Cells[2].Value.ToString() == "")
+                    {
+                        SalesGV.Rows[i].Cells[2].Value = 0;
+                    }
+                    else
+                    {
+                        q = Convert.ToInt32(SalesGV.Rows[i].Cells[2].Value);
+                    }
+
+                    //금액
+                    if ((string)SalesGV.Rows[i].Cells[3].Value.ToString() == "")
+                    {
+                        SalesGV.Rows[i].Cells[3].Value =
+                        0;
+                    }
+                    else
+                    {
+                        SalesGV.Rows[i].Cells[3].Value = q * p; // < --이 부분에서 StackOverflow (무한루프)
+                    }
+
+                    //SalesGV.Rows[i].Cells[3].Value = q * p;
+
+                    //VAT
+                    if ((string)SalesGV.Rows[i].Cells[4].Value.ToString() == "")
+                    {
+                        SalesGV.Rows[i].Cells[4].Value = 0;
+                    }
+                    else
+                    {
+                        SalesGV.Rows[i].Cells[4].Value = Convert.ToInt32(((q * p) * 1.1) - (q * p));
+                    }
+                    //SalesGV.Rows[i].Cells[4].Value = v;
+
+                    //합계
+                    if ((string)SalesGV.Rows[i].Cells[5].Value.ToString() == "")
+                    {
+                        SalesGV.Rows[i].Cells[5].Value = 0;
+                    }
+                    else
+                    {
+                        SalesGV.Rows[i].Cells[5].Value = Convert.ToInt32((q * p) * 1.1);
+                    }
+                    //SalesGV.Rows[i].Cells[5].Value = t;
+                }
+            }
+        }
+    }
+}
